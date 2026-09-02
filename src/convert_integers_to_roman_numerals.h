7b7eb8e9064348cabdbcfbe67c52cc1ff245f3a8
Ian Lanham <ian.lanham2@sky.uk>
2026-09-02 14:48:54 +0100

    introduce prameter numeralValue to addNumeral

--- src/convert_integers_to_roman_numerals.h
+++ src/convert_integers_to_roman_numerals.h
@@ -1,16 +1,14 @@
 #include <string>
 
+void addNumeral(int &number, std::string &romanNumeral);
+
 std::string convertIntToRoman(int number)
 {
     std::string romanNumeral{};
 
-    if (number >= 10)
-    {
-        romanNumeral += "X";
-        number -= 10;
-    }
+    addNumeral(number, romanNumeral);
 
-    if (number == 9)
+    if (number >= 9)
     {
         romanNumeral += "IX";
         number -= 9;
@@ -22,11 +20,20 @@ std::string convertIntToRoman(int number)
         number -= 5;
     }
 
-    if (number == 4)
+    if (number >= 4)
     {
         romanNumeral += "IV";
         number -= 4;
     }
 
     return (romanNumeral + std::string(number,'I'));
+}
+
+void addNumeral(int &number, std::string &romanNumeral)
+{
+    if (number >= 10)
+    {
+        romanNumeral += "X";
+        number -= 10;
+    }
 }
